a470c683111ec2272cb28b07a425f4d674b094e6
Eugene <127371992+eabigails@users.noreply.github.com>
2024-06-11 11:24:49 +0700

    Add files via upload

---
+++ storage/framework/views/3ea7f0f28531b6dcda45ef8d59117d94.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" class="light scroll-smooth group" data-layout="vertical" data-sidebar="light" data-sidebar-size="lg" data-mode="light" data-topbar="light" data-skin="default" data-navbar="sticky" data-content="fluid" dir="ltr">
+<head>
+
+    <meta charset="utf-8">
+    <title>404 | Queen of Thrift</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta content="Minimal Admin & Dashboard Template" name="description">
+    <meta content="StarCode Kh" name="author">
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="<?php echo e(URL::to('assets/images/favicon.ico')); ?>">
+    <!-- Layout config Js -->
+    <script src="<?php echo e(URL::to('assets/js/layout.js')); ?>"></script>
+    <!-- StarCode CSS -->
+    <link rel="stylesheet" href="<?php echo e(URL::to('assets/css/starcode2.css')); ?>">
+</head>
+
+<body class="flex items-center justify-center min-h-screen py-16 bg-cover bg-auth-pattern dark:bg-auth-pattern-dark font-public">
+
+    <?php echo $__env->yieldContent('content'); ?>
+
+    <script src='<?php echo e(URL::to('assets/libs/choices.js/public/assets/scripts/choices.min.js')); ?>'></script>
+    <script src="<?php echo e(URL::to('assets/libs/%40popperjs/core/umd/popper.min.js')); ?>"></script>
+    <script src="<?php echo e(URL::to('assets/libs/tippy.js/tippy-bundle.umd.min.js')); ?>"></script>
+    <script src="<?php echo e(URL::to('assets/libs/simplebar/simplebar.min.js')); ?>"></script>
+    <script src="<?php echo e(URL::to('assets/libs/prismjs/prism.js')); ?>"></script>
+    <script src="<?php echo e(URL::to('assets/libs/lucide/umd/lucide.js')); ?>"></script>
+    <script src="<?php echo e(URL::to('assets/js/starcode.bundle.js')); ?>"></script>
+</body>
+
+</html><?php /**PATH C:\xampp\htdocs\alpcommerce\resources\views/layouts/error.blade.php ENDPATH**/ ?>
